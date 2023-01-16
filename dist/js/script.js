f326e91ad04d68354196a82aa979faf1f4c2a071
Marine Darbinyan <darbimar@mail.ru>
2023-01-16 15:57:40 +0700

    Added tabs with JS

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,44 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabsConten() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show');
+      item.classList.remove('fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabsContent(i = 0) {
+    tabsContent[i].classList.add('show');
+    tabsContent[i].classList.add('fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabsConten();
+  showTabsContent();
+  tabsParent.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabsConten();
+          showTabsContent(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
