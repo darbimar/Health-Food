f3daaa2a3f408c4da70bab692beb24738d48e622
Marine Darbinyan <darbimar@mail.ru>
2023-01-19 19:20:50 +0700

    Added burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -103,5 +103,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
+    //Burger 
+    const burger = document.querySelector('.burger'),
+    header = document.querySelector('header'),
+    body = document.querySelector('body');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active');
+        header.classList.toggle('header__mobile');
+        body.classList.toggle('no-scroll');
+    });
+
+
     
 });
+
