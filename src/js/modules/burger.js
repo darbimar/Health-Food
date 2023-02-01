eb617dffddb3a0da3531d06d4082cfd8a98fc168
Marine Darbinyan <darbimar@mail.ru>
2023-02-01 17:19:26 +0700

    Added modules

---
+++ src/js/modules/burger.js
@@ -0,0 +1,13 @@
+function burger() {
+    const burger = document.querySelector('.burger'),
+    header = document.querySelector('header'),
+    body = document.querySelector('body');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active');
+        header.classList.toggle('header__mobile');
+        body.classList.toggle('no-scroll');
+    });
+}
+
+module.exports = burger;
---
+++ src/js/modules/modal.js
@@ -0,0 +1,48 @@
+function modal() {
+        //Modal
+
+    const modalOpenButton = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal');
+
+    modalOpenButton.addEventListener('click', openModal);
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal || event.target.getAttribute('data-close') == '') {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 10000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+}
+
+module.exports = modal;
